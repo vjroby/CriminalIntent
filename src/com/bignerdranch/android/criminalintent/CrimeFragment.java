8b1b7bcdc25a6a756e572d42c9879015e73e4573
vjroby <vjroby@gmail.com>
2014-09-02 08:11:41 +0300

    format date

--- src/com/bignerdranch/android/criminalintent/CrimeFragment.java
+++ src/com/bignerdranch/android/criminalintent/CrimeFragment.java
@@ -1,9 +1,11 @@
 package com.bignerdranch.android.criminalintent;
 
+import java.util.Date;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,6 +21,7 @@ public class CrimeFragment extends Fragment {
 	private EditText mTitleField;
 	private Button mDateButton;
 	private CheckBox mSolvedCheckBox;
+	private DateFormat mDateFormat = new DateFormat();;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState){
@@ -48,7 +51,13 @@ public class CrimeFragment extends Fragment {
 		});
 		
 		mDateButton = (Button)v.findViewById(R.id.crime_date);
-		mDateButton.setText(mCrime.getDate().toString());
+		
+		Date crimeDate = mCrime.getDate();
+
+		CharSequence pattern = "EEEE, MMM dd, yyyy";   
+   		CharSequence dateFormat = android.text.format.DateFormat.format(pattern, crimeDate);
+   				
+		mDateButton.setText(dateFormat);
 		mDateButton.setEnabled(false);
 		
 		mSolvedCheckBox = (CheckBox)v.findViewById(R.id.crime_solved);
@@ -64,4 +73,5 @@ public class CrimeFragment extends Fragment {
 		
 		return v;
 	}
+
 }
